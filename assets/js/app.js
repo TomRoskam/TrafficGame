7020f40168f0d5e49d4491e748f7af422efa63e9
TomRoskam <tomroskam38@gmail.com>
2018-12-11 11:20:55 +0100

    map

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,12 @@
 console.log("I LIVE");
+window.onload = function () {
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+    function preload() {
+        game.load.spritesheet('map', './assets/images/CityPack/Spritesheet/map.png', 81, 65, 16);
+    }
+    function create() {
+        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+    }
+};
 //# sourceMappingURL=app.js.map
--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,6 @@
 console.log("I LIVE");
 
-window.onload = function() {
+window.onload = function () {
 
     //  Note that this html file is set to pull down Phaser 2.5.0 from the JS Delivr CDN.
     //  Although it will work fine with this tutorial, it's almost certainly not the most current version.
@@ -8,17 +8,19 @@ window.onload = function() {
 
     var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
 
-    function preload () {
+    function preload() {
 
-        game.load.image('logo', 'phaser.png');
+        game.load.spritesheet('map', './assets/images/CityPack/Spritesheet/map.png', 16, 16, 16);
 
     }
 
-    function create () {
+    function create() {
 
         var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
         logo.anchor.setTo(0.5, 0.5);
 
-    }
+        mysprite = this.game.add.sprite(15, 30, 'myguy');
+        mysprite.frame = 3;
 
-};
+    }
+}
