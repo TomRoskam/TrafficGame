e6497e9fbeb05cf3dae8e5cc8d1237575754252c
TomRoskam <tomroskam38@gmail.com>
2018-12-13 09:55:52 +0100

    mapAndEntities

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,13 @@
 console.log("I LIVE");
 window.onload = function () {
-    var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, './assets/images/grass.png', { preload: preload, create: create });
+    var game = new Phaser.Game(window.innerWidth, (24960 * 0.127) - 550, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
     function preload() {
-        game.load.spritesheet('map', './assets/images/CityPack/Spritesheet/map.png', 16, 16, -1, 0, 1, 0);
+        game.load.image('map', './assets/map/map.jpg');
     }
     function create() {
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'map', 710);
-        logo.anchor.setTo(0.5, 0.5);
-        game.stage.backgroundColor = "#43a011";
+        console.log("test");
+        var map = game.add.image(0, -550, 'map');
+        map.scale.setTo(0.127, 0.127);
     }
 };
 //# sourceMappingURL=app.js.map
--- src/game.ts
+++ src/game.ts
@@ -6,22 +6,23 @@ window.onload = function () {
     //  Although it will work fine with this tutorial, it's almost certainly not the most current version.
     //  Be sure to replace it with an updated version before you start experimenting with adding your own code.
 
-    var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, './assets/images/grass.png', { preload: preload, create: create });
+    var game = new Phaser.Game(window.innerWidth, (24960*0.127)-550, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
 
     function preload() {
-
-        game.load.spritesheet('map', './assets/images/CityPack/Spritesheet/map.png', 16, 16, -1, 0, 1, 0);
-
+    
+        game.load.image('map', './assets/map/map.jpg');
+        
+    
     }
-
+    
     function create() {
-
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'map', 710);
-        logo.anchor.setTo(0.5, 0.5);
-
-        game.stage.backgroundColor = "#43a011"
-        // var mysprite = this.game.add.sprite(15, 30, 'map');
-        // mysprite.frame = 3;
-
+    
+        //  This simply creates an Image using the picture we loaded above and positions it at 100 x 100
+    
+        //  The difference between an Image and a Sprite is that you cannot animate or add a physics body to an Image
+        console.log("test");
+        var map = game.add.image(0, -550, 'map');map.scale.setTo(0.127,0.127);
+        
+    
     }
 }
