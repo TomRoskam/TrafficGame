17fd6819fc92ca81e522ac620c1e17cfc40231f8
GDB99 <Gijsdebruine99@gmail.com>
2018-12-20 13:27:31 +0100

    Init

---
+++ Assets/FollowCar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCar : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform carTransform;
+
+    [SerializeField]
+    private float followSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector3.Lerp(
+            transform.position,
+            carTransform.position - carTransform.forward * 0.5f + new Vector3(0,0.25f   ,0),
+            followSpeed);
+        transform.LookAt(carTransform,carTransform.up);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private WheelCollider[] wheels;
+
+	//Base mobility values
+	[SerializeField]
+	private float maxTorque = 20;
+	private float steeringMod = 10;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {;
+	}
+
+	void FixedUpdate () {
+		this.wheels[2].motorTorque = maxTorque * Input.GetAxis("Vertical");
+		this.wheels[3].motorTorque = maxTorque * Input.GetAxis("Vertical");
+
+		this.wheels[0].steerAngle = steeringMod * Input.GetAxis("Horizontal");
+		this.wheels[1].steerAngle = steeringMod * Input.GetAxis("Horizontal");
+	}
+}
