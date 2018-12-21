9d8dc47a296462affc24e5359ddffcd4c10af374
GDB99 <Gijsdebruine99@gmail.com>
2018-12-21 13:25:31 +0100

    Commiteru the Shitteru

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour {
 
 	[SerializeField]
@@ -12,12 +13,17 @@ public class PlayerController : MonoBehaviour {
 	private float maxTorque = 20;
 	[SerializeField]
 	private float steeringMod = 10;
+
+	[SerializeField]
+	private float testYOffset = 0;
     private Vector3 startPosition;
+	private Rigidbody rb;
 	private Quaternion startRotation;
 
 
 	// Use this for initialization
 	void Start () {
+		this.rb = GetComponent<Rigidbody>();
         this.startPosition = this.transform.position;
 		this.startRotation = this.transform.rotation;
 	}
@@ -27,6 +33,9 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
+
+		this.rb.centerOfMass = new Vector3(0, testYOffset, 0);
+
 		if(Input.GetKeyDown(KeyCode.U)) {
 			this.transform.position = this.startPosition;
 			this.transform.rotation = this.startRotation;
@@ -37,5 +46,8 @@ public class PlayerController : MonoBehaviour {
 
 		this.wheels[0].steerAngle = steeringMod * Input.GetAxis("Horizontal");
 		this.wheels[1].steerAngle = steeringMod * Input.GetAxis("Horizontal");
+
+		this.wheels[2].brakeTorque = maxTorque * Input.GetAxis("Jump");
+		this.wheels[3].brakeTorque = maxTorque * Input.GetAxis("Jump");
 	}
 }
