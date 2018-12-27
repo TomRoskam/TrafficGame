d36688bbdf4f5f44b533449658ccc8b53315bae8
TomRoskam <tomroskam38@gmail.com>
2018-12-27 11:42:28 +0100

    MapPuntenSysteem

---
+++ Assets/Score.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public Transform jeep;
+    public Text scoreText;
+    public float scoreInt;
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (jeep.position.z >= 50 && jeep.position.z <= 62 && jeep.position.x >= 42 && jeep.position.x <=54)
+        {
+            Debug.Log("Xmin = 42, Xmax = 54, Zmin = 50, Zmax = 62");
+
+            scoreInt = scoreInt + 1;
+
+            scoreText.text = scoreInt.ToString();
+            Debug.Log(scoreInt.ToString());
+        }
+    }
+}
