20ac66bda89b31050bd3da23f3c4f242e177764b
TomRoskam <tomroskam38@gmail.com>
2018-12-30 11:57:46 +0100

    TreePlacement

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -9,17 +9,16 @@ public class Score : MonoBehaviour
     public Transform jeep;
     public Text scoreText;
     public float scoreInt;
+    public float addScore;
     bool CHECKER = true;
     bool CHECKER2 = true;
     bool CHECKER3 = true;
     bool CHECKER4 = true;
 
-
-
-
     // Start is called before the first frame update
     private void Start()
     {
+       addScore = 100;
 
     }
     // Update is called once per frame
@@ -34,7 +33,7 @@ public class Score : MonoBehaviour
     {
         if (jeep.position.z >= 50 && jeep.position.z <= 62 && jeep.position.x >= 42 && jeep.position.x <= 54 && CHECKER == true)
         {
-            scoreInt = scoreInt + 100;
+            scoreInt = scoreInt + addScore;
             CHECKER = false;
             scoreText.text = scoreInt.ToString();
             Debug.Log(scoreInt.ToString());
@@ -42,7 +41,7 @@ public class Score : MonoBehaviour
 
         else if (jeep.position.z >= 193 && jeep.position.z <= 205 && jeep.position.x >= 408 && jeep.position.x <= 416 && CHECKER2 == true)
         {
-            scoreInt = scoreInt + 100;
+            scoreInt = scoreInt + addScore;
             CHECKER2 = false;
             scoreText.text = scoreInt.ToString();
             Debug.Log(scoreInt.ToString());
@@ -50,7 +49,7 @@ public class Score : MonoBehaviour
 
         else if (jeep.position.z >= 240 && jeep.position.z <= 254 && jeep.position.x >= 160 && jeep.position.x <= 164 && CHECKER3 == true)
         {
-            scoreInt = scoreInt + 100;
+            scoreInt = scoreInt + addScore;
             CHECKER3 = false;
             scoreText.text = scoreInt.ToString();
             Debug.Log(scoreInt.ToString());
@@ -58,7 +57,7 @@ public class Score : MonoBehaviour
 
         else if (jeep.position.z >= 373 && jeep.position.z <= 375 && jeep.position.x >= 354 && jeep.position.x <= 367 && CHECKER4 == true)
         {
-            scoreInt = scoreInt + 100;
+            scoreInt = scoreInt + addScore;
             CHECKER4 = false;
             scoreText.text = scoreInt.ToString();
             Debug.Log(scoreInt.ToString());
