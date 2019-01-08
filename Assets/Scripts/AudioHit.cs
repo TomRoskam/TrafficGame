7b95b71671aa8653905610406f7a4a5aa74ac4fb
Tom de Bruijn <tomokp@users.noreply.github.com>
2019-01-08 10:06:52 +0100

    lighting_fixed

---
+++ Assets/Scripts/AudioHit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioHit : MonoBehaviour
+{
+    public AudioSource HitSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HitSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject)
+        {
+            HitSound.Play();
+        }
+    }
+}
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -30,85 +30,46 @@ public class Score : MonoBehaviour
     {
         if (jeep.position.z >= 50 && jeep.position.z <= 62 && jeep.position.x >= 42 && jeep.position.x <= 54 && CHECKER == true)
         {
-            if(MakkelijkScript.MakkelijkClicked == true)
-            {
-                scoreInt = scoreInt + MakkelijkScript.MakkelijkPunten;
-            } else if (NormaalScript.NormaalClicked == true)
-            {
-                scoreInt = scoreInt + NormaalScript.NormaalPunten;
-            } else if (MoeilijkScript.MoeilijkClicked == true)
-            {
-                scoreInt = scoreInt + MoeilijkScript.MoeilijkPunten;
-            }
-
+            PointAwarding();
             CHECKER = false;
-            Debug.Log("SCORE" + scoreInt);
-            scoreText.text = scoreInt.ToString();
-            Debug.Log(scoreInt.ToString());
-
         }
 
         else if (jeep.position.z >= 193 && jeep.position.z <= 205 && jeep.position.x >= 408 && jeep.position.x <= 416 && CHECKER2 == true)
         {
-            if (MakkelijkScript.MakkelijkClicked == true)
-            {
-                scoreInt = scoreInt + MakkelijkScript.MakkelijkPunten;
-            }
-            else if (NormaalScript.NormaalClicked == true)
-            {
-                scoreInt = scoreInt + NormaalScript.NormaalPunten;
-            }
-            else if (MoeilijkScript.MoeilijkClicked == true)
-            {
-                scoreInt = scoreInt + MoeilijkScript.MoeilijkPunten;
-            }
-
+            PointAwarding();
             CHECKER2 = false;
-            Debug.Log("SCORE" + scoreInt);
-            scoreText.text = scoreInt.ToString();
-            Debug.Log(scoreInt.ToString());
         }
 
         else if (jeep.position.z >= 240 && jeep.position.z <= 254 && jeep.position.x >= 160 && jeep.position.x <= 164 && CHECKER3 == true)
         {
-            if (MakkelijkScript.MakkelijkClicked == true)
-            {
-                scoreInt = scoreInt + MakkelijkScript.MakkelijkPunten;
-            }
-            else if (NormaalScript.NormaalClicked == true)
-            {
-                scoreInt = scoreInt + NormaalScript.NormaalPunten;
-            }
-            else if (MoeilijkScript.MoeilijkClicked == true)
-            {
-                scoreInt = scoreInt + MoeilijkScript.MoeilijkPunten;
-            }
-
+            PointAwarding();
             CHECKER3 = false;
-            Debug.Log("SCORE" + scoreInt);
-            scoreText.text = scoreInt.ToString();
-            Debug.Log(scoreInt.ToString());
         }
 
         else if (jeep.position.z >= 373 && jeep.position.z <= 375 && jeep.position.x >= 354 && jeep.position.x <= 367 && CHECKER4 == true)
         {
-            if (MakkelijkScript.MakkelijkClicked == true)
-            {
-                scoreInt = scoreInt + MakkelijkScript.MakkelijkPunten;
-            }
-            else if (NormaalScript.NormaalClicked == true)
-            {
-                scoreInt = scoreInt + NormaalScript.NormaalPunten;
-            }
-            else if (MoeilijkScript.MoeilijkClicked == true)
-            {
-                scoreInt = scoreInt + MoeilijkScript.MoeilijkPunten;
-            }
-
+            PointAwarding();
             CHECKER4 = false;
-            Debug.Log("SCORE" + scoreInt);
-            scoreText.text = scoreInt.ToString();
-            Debug.Log(scoreInt.ToString());
         }
+    }
+
+    public void PointAwarding()
+    {
+        if (MakkelijkScript.MakkelijkClicked == true)
+        {
+            scoreInt = scoreInt + MakkelijkScript.MakkelijkPunten;
+        }
+        else if (NormaalScript.NormaalClicked == true)
+        {
+            scoreInt = scoreInt + NormaalScript.NormaalPunten;
+        }
+        else if (MoeilijkScript.MoeilijkClicked == true)
+        {
+            scoreInt = scoreInt + MoeilijkScript.MoeilijkPunten;
+        }
+
+        Debug.Log("SCORE" + scoreInt);
+        scoreText.text = scoreInt.ToString();
+        Debug.Log(scoreInt.ToString());
     }
 }
